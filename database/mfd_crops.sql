6d74101009a2817801b1a17b5b5af9ee841c288a
Ellie Parker <ellie@i-parker.com>
2023-04-05 13:25:50 -0700

    Removed old table constructor sql files

--- database/mfd_crops.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `mfd_crops`;
-
-CREATE TABLE `mfd_crops` (
-  `crop_id` int NOT NULL AUTO_INCREMENT,
-  `crop_name` varchar(256) NOT NULL,
-  `crop_description` varchar(2048) DEFAULT NULL,
-  `crop_info_link` varchar(2048) DEFAULT NULL,
-  PRIMARY KEY (`crop_id`)
-);
