c29042792eb65af03dd3ce70ff79a582e80dc5f7
Erika Mendoza <erikagmendoz@gmail.com>
2023-05-01 20:35:02 -0700

    SQL script for mfd_crops_in_recipe table.

---
+++ database/MFD_crops_in_recipe.sql
@@ -0,0 +1,65 @@
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('1', '6');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('1', '3');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('1', '11');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('1', '21');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('1', '14');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('2', '12');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('2', '27');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('2', '13');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('3', '21');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('3', '22');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('3', '11');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('4', '8');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('4', '11');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('4', '27');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('4', '17');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('4', '12');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('5', '15');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('5', '11');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('5', '12');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('5', '18');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('6', '11');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('6', '12');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('6', '21');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('6', '28');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('6', '29');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('7', '11');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('7', '19');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('8', '14');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('8', '11');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('9', '11');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('9', '27');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('9', '20');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('9', '12');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('10', '21');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('10', '30');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('10', '27');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('11', '12');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('11', '13');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('12', '4');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('13', '31');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('14', '21');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('14', '28');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('14', '13');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('14', '6');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('14', '12');
+
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('15', '27');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('15', '12');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('15', '8');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('15', '7');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('15', '15');
+INSERT INTO mfd_crops_in_recipe (recipe_id, crop_id) VALUES ('15', '22');
