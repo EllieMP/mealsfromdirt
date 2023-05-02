bc5777f1479de386aa0d82c7b46c310590fecaf7
Ellie Parker <87090093+EllieMP@users.noreply.github.com>
2023-05-02 16:38:18 -0700

    Update crops_router.js

--- api/routes/crops_router.js
+++ api/routes/crops_router.js
@@ -7,6 +7,4 @@ cropsRouter.get('/:crop_id', cropsController.getCropByID);
 
 cropsRouter.put('/update/:crop_id/:crop_name', cropsController.updateCropNameByID);
 
-cropsRouter.post('/new/:crop_name/:crop_description/:crop_image_link', cropsController.addCrop);
-
 module.exports = cropsRouter;
