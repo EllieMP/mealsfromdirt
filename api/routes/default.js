fd4de55c68f473db8f701a5595e9f27509c42763
Ellie Parker <ellie@i-parker.com>
2023-05-02 19:29:22 -0700

    Fixed routes

--- api/routes/default.js
+++ api/routes/default.js
@@ -4,6 +4,8 @@ const cropBiomeRouter = require('./crop_biome_router.js');
 const cropsInRecipeRouter = require('./crops_in_recipe_router.js');
 const recipesRouter = require('./recipes_router.js');
 const cordToBiomeRouter = require('./cordinate_to_biome_router.js');
+const cordsToCropsRouter = require('./cordinate_to_crops_router.js');
+const cordsToRecipesRouter = require('./cordinate_to_recipes_router.js');
 
 const defaultRouter = require('koa-router')({
     prefix: '/api/v1'
@@ -19,6 +21,8 @@ defaultRouter.use(cropBiomeRouter.routes());
 defaultRouter.use(cropsInRecipeRouter.routes());
 defaultRouter.use(recipesRouter.routes());
 defaultRouter.use(cordToBiomeRouter.routes());
+defaultRouter.use(cordsToCropsRouter.routes());
+defaultRouter.use(cordsToRecipesRouter.routes());
 
 module.exports = api => {
     api.use(
